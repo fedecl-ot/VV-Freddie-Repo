425075163dcb6a1464d3cb5227a371855cac0a66
Federico Cuelho <federico.cuelho@onetree.com>
2022-04-14 13:05:17 -0300

    Added SQL example

---
+++ projects/Training presentations/GRM SA/sqlScript.sql
@@ -0,0 +1,10 @@
+SELECT
+    [Display Value],
+    [Options Value]
+FROM
+    [Nombre Del Formulario]
+WHERE
+    [List Name] = 'Nombre De La Lista'
+    AND [Status] = 'Enabled'
+ORDER BY
+    [Position]
