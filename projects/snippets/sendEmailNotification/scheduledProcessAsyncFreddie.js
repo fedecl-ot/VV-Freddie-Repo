b852148312f89eec60fe7b4391da58702f6a3a8e
Federico Cuelho <federico.cuelho@onetree.com>
2022-06-22 15:59:59 -0300

    Modified credentials

--- projects/snippets/sendEmailNotification/scheduledProcessAsyncFreddie.js
+++ projects/snippets/sendEmailNotification/scheduledProcessAsyncFreddie.js
@@ -49,15 +49,14 @@ module.exports.main = async function (vvClient, response, token) {
 
     const formTemplateName = 'Milestone11';
 
-    // Place all variables in the 'Configurable Variables' section.
     const scheduledProcessName = 'scheduledProcessAsyncFreddie';
 
     /***********************
-        Email Variables
-    ************************/
+    Email Variables
+************************/
 
     // Email addresses list to send notification.
-    let testEmailList = 'email1@gmail.com, email2@gmail.com';
+    const testEmailList = 'email1@gmail.com, email2@gmail.com';
 
     // On 'true' sends the notification to all VaultAccess users email addresses.
     const useTestEmails = false;
@@ -172,9 +171,39 @@ module.exports.main = async function (vvClient, response, token) {
         return vvClientRes;
     }
     async function sendEmailNotification(processStatusMsj) {
+        /*
+      Script Name:   sendEmailNotification
+      Customer:      N/A
+      Purpose:       Auxiliary function intended to be used in Scheduled Processes to send an email with a summary of the process result and error logs.
+      Parameters:    The following are parameters that need to be passed into this auxiliary script.
+                     processStatusMsj - Process status message shown within the email body.
+ 
+      Return Object:
+                     - N/A.
+      Psuedo code:
+                     1. Call LibGroupGetGroupUserEmails library to fetch VaultAcces user group data.
+                     2. Loop every user data to get their email addresses to save it as a string of emails.
+                     3. Check for errors within the log.
+                     4. Determine email recipients.
+                     5. Build email structure.
+                     6. Send email.
+ 
+      Date of Dev:   02/22/2022
+      Last Rev Date: 02/23/2022
+ 
+      Revision Notes:
+      02/22/2022 - FEDERICO CUELHO:     First Setup of the script.
+      02/23/2022 - FEDERICO CUELHO:     - Added comments.
+                                        - Added parsing and error handling functions.
+                                        - Replace the for iteration for a map function.
+                                        - Relocated variables.
+                                        - Modified chained variables into single variable declarations.
+                                        - Modified email subject structure.
+     */
+
         logger.info('Entered sendEmailNotification process.');
 
-        // Get process date and time.
+        // Get scheduled process execution date and time.
         const localISODate = new Date().toISOString().substring(0, 10);
         const localTime = new Date().toTimeString();
 
@@ -189,10 +218,7 @@ module.exports.main = async function (vvClient, response, token) {
             },
         ];
 
-        // Error variable.
-        let errorItem = '';
-
-        // FETCH THE GROUP USERS' EMAILS.
+        // FETCH THE GROUP USER DATA.
         const resVisualAccessUsers = await vvClient.scripts
             .runWebService('LibGroupGetGroupUserEmails', groupsParamObj)
             .then((res) => parseRes(res, shortDescription))
@@ -200,10 +226,13 @@ module.exports.main = async function (vvClient, response, token) {
             .then((res) => checkDataPropertyExists(res, shortDescription))
             .then((res) => checkDataIsNotEmpty(res, shortDescription));
 
-        // LOOPS EVERY USER TO GET THEIR EMAIL ADDRESSES TO SAVE IT AS A STRING OF EMAILS.
+        // LOOPS EVERY USER  DATA TO GET THEIR EMAIL ADDRESSES TO SAVE IT AS A STRING OF EMAILS.
         resVisualAccessUsers.data[2].map(async (userData) => {
             emailList += userData['emailAddress'] + ',';
         });
+
+        // Error variable.
+        let errorItem = '';
 
         // CONTROL TO SHOW ERRORS LOG LIST.
         if (errorLog.length > 0) {
@@ -224,24 +253,24 @@ module.exports.main = async function (vvClient, response, token) {
             recipients: emailList,
             subject: `${scheduledProcessName}, ${localISODate}, ${localTime}`,
             body: `
-                Hello,<br>
-                <br>
-                Here is your scheduled process report:
-                <ul>
-                    <li>Status: <strong>${processStatusMsj}</strong>.</li>
-                    <li>Date: ${localISODate}.</li>
-                    <li>Time: ${localTime}.</li>
-                </ul>
-                <br>
-                Errors found:
-                <ul>
-                    <li>${errorItem}</li>
-                </ul>
-                <br>
-                Regards,<br>
-                <br>
-                VisualVault.
-            `,
+            Hello,<br>
+            <br>
+            Here is your scheduled process report:
+            <ul>
+                <li>Status: <strong>${processStatusMsj}</strong>.</li>
+                <li>Date: ${localISODate}.</li>
+                <li>Time: ${localTime}.</li>
+            </ul>
+            <br>
+            Errors found:
+            <ul>
+                <li>${errorItem}</li>
+            </ul>
+            <br>
+            Regards,<br>
+            <br>
+            VisualVault.
+        `,
         };
 
         // SENDS EMAIL.
@@ -281,7 +310,7 @@ module.exports.main = async function (vvClient, response, token) {
 
             // CREATES NEW OBJECT WITH UPDATED SAVED FORM CONTROL VALUES
             const formFieldsToUpdate = {
-                name: 'Fedi',
+                name: 'Fede',
                 lastname: 'Cuelho',
             };
 
