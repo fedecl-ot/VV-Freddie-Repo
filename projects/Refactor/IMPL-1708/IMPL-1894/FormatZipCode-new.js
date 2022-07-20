d0beb278729d259e397dfdb8183fcab059b0b1f8
Federico Cuelho <federico.cuelho@onetree.com>
2022-07-20 17:47:13 -0300

    Add first code review on IMPL-1894

---
+++ projects/Refactor/IMPL-1708/IMPL-1894/FormatZipCode-new.js
@@ -0,0 +1,52 @@
+/*
+	Script Name:   FormatZipCode
+	Customer:      VisualVault
+	Purpose:       The purpose of this function is to take a zip code entered in any format and format it into a standard of either ##### or #####-#### (zip + 4). 
+	Parameters:    The following represent variables passed into the function:
+				          -passedString: string value that represents the zip code
+				  
+	Return Value:  The following represents the value being returned from this function:
+                  -passedString: string value (In case format is not applyable)
+                  -formatedZipCode: string value   (In case format is applyable)
+    
+	Date of Dev: unknown
+	Last Rev Date: 07/11/2022
+	Revision Notes:
+        unknown - unknown: Initial creation of the business process.
+				07/11/2022 - Franco Petosa Ayala: update to ES6.
+*/
+
+// initialize in false value
+let notValid = false;
+
+let stringArr = passedString.split("")
+
+//build the string array based only on number digits
+stringArr = stringArr.filter(digit => {
+  if(!isNaN(digit) && digit != " "){
+    return true
+  }else if(digit === '-'){
+    return false
+  }else{
+    //if the zip code contains a digit that is not neither a number nor a hyphen it means is not valid 
+    notValid = true;
+    return false
+  }
+})
+
+//verify the passed zip code is valid
+if(notValid){
+  return passedString
+}else if(stringArr.length !== 5 && stringArr.length !== 9){
+  return passedString
+}
+
+//if is a comepleted zip code of 9 valid digits, add the hyphen right after the first five digits
+if (stringArr.length === 9) {  
+  stringArr.splice(5,0,"-")
+}
+
+//build-up the string formatedZipCode
+const formatedZipCode = stringArr.join("");
+
+return formatedZipCode
